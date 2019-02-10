5894f3e406581b4d18bd6c21c5f1447a4fd6732e
victor obije <konigeobije@gmail.com>
2019-02-10 14:08:13 +0100

    [feature 160910643] get menu funtionality

---
+++ src/actions/getMenuActions.js
@@ -0,0 +1,23 @@
+import  { GET_MENU, GET_MENU_ERROR } from './types';
+import axios from 'axios';
+import orders from '../component/orders';
+
+const GetMenuAction = () => (dispatch) => {
+
+return axios.get('http://localhost:8000/api/v1/menu')
+        .then((response) => {
+        console.log(response.data.data);
+            dispatch({
+                type: GET_MENU,
+                payload: response.data.data
+            });
+    }).catch((error)=> {
+        console.log(error.message);
+        dispatch({
+            type: GET_MENU_ERROR,
+            payload:'failure you have an error'
+            })
+    });  
+}
+
+export default GetMenuAction;
